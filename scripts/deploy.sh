c59c72f4a8468362776b191947ae101082282c87
Allard van Helbergen <allard@brandwatch.com>
2015-03-29 20:03:22 +0200

    #6 Improve deploy script to fail on error.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 
+set -e
+
+
+TARGET_DIR=../go-backup/go-$(date +"%Y%m%d-%H%M%S")
+
+
 function title {
     echo
     echo "# $1..."
@@ -10,27 +16,26 @@ function title {
 
 # TODO(allard): How do you stop the app without killing everything?!
 # Stop forever first.
-# title "Stopping the App"
+# title "Setting phasers to stun"
 # forever stop 0
 
 # Copy current files to back up directory   .
-title "Backing up files"
-TARGET_DIR=../go-backup/go-$(date +"%Y%m%d-%H%M%S")
+title "Backing up files to Flight Control"
 printf "Target directory: %s\n" $TARGET_DIR
 cp -Rf . $TARGET_DIR
 
 # Pull newest version from Git and install.
-title "Pulling newest version"
+title "Pulling newest version from Space Center"
 git pull origin master
 
-title "Running set up"
+title "Running Initialisation Protocols"
 ./scripts/setup.sh
 
 # Restart the app.
-title "Restarting the App"
+title "Restarting Go"
 forever restartall
 
 # Finish.
-title "Done"
-echo "Hooray! \o/"
+title "Mission Complete"
+echo "Ready to boldly go, where no link has gone before!"
 exit 0
