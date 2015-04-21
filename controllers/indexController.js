07cf2386cc158eeab7601268a5bdb2b55ca14b6d
Allard van Helbergen <allard@brandwatch.com>
2015-04-21 13:56:36 +0200

    #6 Fix redirect bug.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -20,11 +20,12 @@ exports.notFound = function(req, res) {
 /**
  * GET: the URL to redirect to based on supplied shortUri.
  */
-exports.redirect = function(req, res) {
-    console.log('Redirecting:', req.goLink);
+exports.redirect = function(req, res, next) {
     if (!req.goLink) {
-        return;
+        next();
     }
+
     // TODO(allard): Analytics would go here.
+    console.log('Redirecting:', req.goLink);
     res.redirect(req.goLink.longUri);
 };
