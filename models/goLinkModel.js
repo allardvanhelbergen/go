4859bb58f3fb6120898d4fcfdb9fe65130c2b9cb
Allard van Helbergen <allard@brandwatch.com>
2015-04-30 00:13:19 +0200

    #14 Add validator for shortUris.

--- models/goLinkModel.js
+++ models/goLinkModel.js
@@ -4,12 +4,24 @@
 var mongoose = require('mongoose');
 
 
+/**
+ * Validates if a shortUri is a permalink (alphanumeric and dashes only).
+ * @param  {String} shortUri The shortUri to validate.
+ * @return {Boolean}         TRUE: shortUri is a permalink.
+ *                           FALSE: shortUri is not a permalink.
+ */
+var validateShortUri = function(shortUri) {
+    return (shortUri.match(/^[a-z0-9\-]+$/)) ? true : false;
+};
+
+
 var GoLinkSchema = new mongoose.Schema({
     shortUri: {
         type: String,
         trim: true,
         required: true,
-        unique: true
+        unique: true,
+        validate: [validateShortUri, 'Is not a valid Go link']
     },
     longUri: {
         type: String,
