019c346a36feeb0ede00631a78efbd29160f8d88
Allard van Helbergen <allard@brandwatch.com>
2015-05-20 09:50:10 +0200

    NOTICKET Add first test with mocha.

---
+++ tests/lib/validatorsTest.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+require('../testHelper');
+var validators = require('../../app/lib/validators');
+
+var ALPHA_NUM_DASH = 'abcdefghijklmnopqrstuvwxyz0123456789-';
+var OTHER_CHAR = '!@#$%^&*()';
+
+
+describe('validators', function() {
+    describe('validateShortUri', function() {
+        it('Validates alphanumeric and dash chars', function() {
+            assert(validators.validateShortUri(ALPHA_NUM_DASH));
+        });
+
+        it('Invalidates other chars', function() {
+            assert.equal(validators.validateShortUri(OTHER_CHAR), false);
+        });
+    })
+});
+
