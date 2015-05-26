6ee802fcc667258eaec0cc0289d07e2a1194b3e2
Allard van Helbergen <allard@brandwatch.com>
2015-05-26 18:10:03 +0200

    #23 Add errors for DB being down.

--- app/lib/db.js
+++ app/lib/db.js
@@ -7,18 +7,44 @@ var winston = require('winston');
 
 
 module.exports.connect = function() {
+    var connected = false;
     var db;
-    var dbUrl = 'mongodb://' + config.mongo.HOSTNAME + '/' + config.mongo.DATABASE + '?auto_reconnect';
+    var DB_URL = 'mongodb://' + config.mongo.HOSTNAME + '/' + config.mongo.DATABASE + '?auto_reconnect';
 
-    mongoose.connect(dbUrl);
+    mongoose.connect(DB_URL);
     db = mongoose.connection;
 
-    db.on('error', function(err) {
-        winston.error('Database connection error:', err);
-        return err;
+
+    // TODO(allard): This can probably do with some smartening up.
+    db.on('close', function(ref) {
+        connected = false;
+        winston.error('Database connection closed.', ref);
     });
 
-    db.once('open', function() {
-        winston.info('Database connection successful at:', dbUrl);
+    db.on('connected', function(ref) {
+        connected = true;
+        winston.info('Database connection connected.', ref);
+    });
+
+    db.on('disconnected', function(ref) {
+        connected = false;
+        winston.log('Database connection disconnected.', ref);
+    });
+
+    db.on('error', function(err) {
+        connected = false;
+        winston.error('Database connection error.');
+        winston.error(err);
+        throw err;
+    });
+
+    db.on('open', function(ref) {
+        connected = true;
+        winston.info('Database connection successful at:', DB_URL);
+    });
+
+    db.on('reconnect', function(ref) {
+        connected = true;
+        winston.info('Database connection reconnect.', ref);
     });
 };
